cf1c8a2adaabe191b56128fb5b9fed1284d2d819
Dev. Pablo H <146978584+devpabloh@users.noreply.github.com>
2024-02-26 09:29:05 -0300

    atulização

--- introdução/primeiroexemplo.js
+++ introdução/primeiroexemplo.js
@@ -1,1 +1,14 @@
 console.log("JavaScript funcionando")
+
+let nome = "Daniel"
+console.log(10+20)
+
+// comentário de uma única linha
+
+/* comentário de múltiplas linhas*/ 
+
+function getNome(reload){
+console.log("")
+}
+
+const name = ""
