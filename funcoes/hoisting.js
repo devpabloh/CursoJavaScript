4926af6643f58f08c9be8c733e7d19923da05f0b
Dev. Pablo H <146978584+devpabloh@users.noreply.github.com>
2024-02-26 22:46:48 -0300

    Update hoisting.js

--- funcoes/hoisting.js
+++ funcoes/hoisting.js
@@ -6,7 +6,7 @@ teste()
 
 
 
-// o hoisting apenas funciona quando estamos chamando uma função que foi declarada, quando estamos falando de atribuição de variável, como estamos vendo aqui em baixo, o mesmo só pode ser chamado após o codificador ler aquela atribuição que foi dada, diferentemente da função declarada, que por ser hosting, você pode chamar antes da função que vai funcionar normalmente.
+// o hoisting apenas funciona quando estamos chamando uma função que foi declarada, quando estamos falando de atribuição de variável, como estamos vendo aqui em baixo, o mesmo só pode ser chamado após o codificador ler aquela atribuição que foi dada, diferentemente da função declarada, que por ser hosting, você pode chamar antes da função que vai funcionar normalmente. A mesma coisa também acontece no uso do let e do const na declaração de váriaveis, mas o Var funciona o hosting.
 
 const teste = function teste (){
     console.log("teste")
