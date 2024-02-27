531fcd04030cc1da337d47be6300ca53223e1601
Dev. Pablo H <146978584+devpabloh@users.noreply.github.com>
2024-02-26 22:45:16 -0300

    Create hoisting.js

---
+++ funcoes/hoisting.js
@@ -0,0 +1,18 @@
+function teste (){
+    console.log("teste")
+}
+
+teste()
+
+
+
+// o hoisting apenas funciona quando estamos chamando uma função que foi declarada, quando estamos falando de atribuição de variável, como estamos vendo aqui em baixo, o mesmo só pode ser chamado após o codificador ler aquela atribuição que foi dada, diferentemente da função declarada, que por ser hosting, você pode chamar antes da função que vai funcionar normalmente.
+
+const teste = function teste (){
+    console.log("teste")
+}
+
+teste()
+console.log(minhaVar)
+var minhaVar = "variável"
+
