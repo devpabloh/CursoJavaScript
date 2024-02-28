c562994e72f7bb25c4daeeb3b98c5ad89acf3940
Dev. Pablo H <146978584+devpabloh@users.noreply.github.com>
2024-02-27 21:56:18 -0300

    Update iife-modal.js

--- funcoes/iife-modal.js
+++ funcoes/iife-modal.js
@@ -1,7 +1,18 @@
 
-function init(){
+/* function init(){
     let isValid = true
     console.log("init modal", isValid)
 
 }
 init()
+ */
+
+(function(){
+    let isValid = false
+    console.log("init modal", isValid)
+
+    function init(){
+        console.log("init do modal")
+    }
+    init()
+})()
