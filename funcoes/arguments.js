9cf0bd905cb859b0a3017cdb3d0aa7e5227806c2
Dev. Pablo H <146978584+devpabloh@users.noreply.github.com>
2024-02-28 23:09:06 -0300

    Update arguments.js

--- funcoes/arguments.js
+++ funcoes/arguments.js
@@ -10,3 +10,15 @@ function somar (){
 console.log(somar(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
 
 /* a variável arguments é uma array-like object (objeto similar a uma array) que contém todos os argumentos passados para uma função. Ela permite que você acesse os argumentos de uma função, independentemente do número de argumentos definidos formalmente na declaração da função. Isso é especialmente útil quando você não sabe quantos argumentos serão passados para a função. */
+
+
+// quando precisar ter acesso a uma função do array like object, não use arrow function, que vai dar erro
+
+const Subtrair = () => {
+    console.log(arguments)
+    let tortal = 0;
+    for(let j = 0; j <arguments.length; j++ ){
+        tortal = arguments[i]
+    }
+    return tortal
+}
