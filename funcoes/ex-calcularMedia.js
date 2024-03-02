0e95b3e82171a93b3ea90efbab215ff7edf65471
Dev. Pablo H <146978584+devpabloh@users.noreply.github.com>
2024-03-01 22:30:57 -0300

    Update ex-calcularMedia.js

--- funcoes/ex-calcularMedia.js
+++ funcoes/ex-calcularMedia.js
@@ -12,3 +12,21 @@ let calcularMedia2 =(n3, n4) =>{
 }
 
 calcularMedia2(8,8)
+
+(function (){
+    function calcularMedia2(){
+        let total2 = 0
+        let qtd = arguments.length
+
+        for(let i=0; i < qtd; i++){
+            if(typeof arguments[i] !== "number"){
+                throw error("Só pode números")
+            }
+            total2 += arguments[i]
+            return total2 / qtd
+        }
+
+    }
+    let media3 = calcularMedia2(2,4)
+
+}())
